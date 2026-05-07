ce0b52fb677d47880313c960d892fdeed17bc3d1
YOG TANDEL <yogtandel.dev@gmail.com>
2026-05-07 22:21:16 +0530

    feat: add initial configuration for Hyprland and Rofi launcher script

--- .config/rofi/launcher.sh
+++ .config/rofi/launcher.sh
@@ -10,7 +10,7 @@
 ## style-1     style-2     style-3     style-4     style-5
 ## style-6     style-7     style-8     style-9     style-10
 
-dir="$HOME/rofi/files/launchers/type-6"
+dir="$HOME/.config/rofi"
 theme='style-6'
 
 ## Run
